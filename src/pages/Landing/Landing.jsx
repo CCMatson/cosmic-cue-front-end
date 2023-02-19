3232c737d9b14c8777fd3272f86a3d27edb0d44c
CCMatson <catherine.matson@gmail.com>
2023-02-19 11:19:34 -0800

    update landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,6 +5,7 @@ const Landing = ({ user }) => {
         <img src="/Logo.png" alt="" />
         <img src="https://i.imgur.com/k4iOvKo.png" alt="logo" />
         <h1>Welcome to Cosmic Cue, {user ? user.name : 'friend'}</h1>
+        <h1>Click the Fortune link above to get your "cosmic cue" or sign in to create, edit, and delete new messages</h1>
       </div>
     </main>
   )
